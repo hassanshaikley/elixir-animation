9840260c8dfcb3ae8585754d4d913f5ff84eae8f
hassan shaikley <hassan.shaikley@gmail.com>
2018-04-14 23:25:14 -0600

    Renamed bunter->main

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Animation.MixProject do
 
 
   defp escript do
-    [main_module: Bunter,
+    [main_module: Main,
      emu_args: "-noinput -elixir ansi_enabled true"]
   end
 
@@ -28,8 +28,6 @@ defmodule Animation.MixProject do
   defp deps do
     [
       {:bunt, "~> 0.1.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
